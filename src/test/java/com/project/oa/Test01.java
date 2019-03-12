890f1d2fe4c999d6546f29a83f417f6c698299f9
zhanghongkai <42404571+q1054733797@users.noreply.github.com>
2019-03-12 16:42:20 +0800

    测试同步

--- src/test/java/com/project/oa/Test01.java
+++ src/test/java/com/project/oa/Test01.java
@@ -11,7 +11,7 @@ import com.project.oa.base.bean.User;
 public class Test01 {
     public static void main(String[] args) {
         User user = new User();
-        user.setName(new String("zhang"));
+        user.setName("zhang");
         String a = "123";
         String b = "zhang";
         System.out.println(user.getName() == b);
